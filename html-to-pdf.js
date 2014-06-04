75c9259ee7dcbd4b406515f7ee49450518feaf79
Sean Wesenberg <wookets@gmail.com>
2014-06-04 18:55:19 -0500

    init - html to pdf
    Signed-off-by: Sean Wesenberg <wookets@gmail.com>

---
+++ html-to-pdf.js
@@ -0,0 +1,37 @@
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var request = require('request');
+
+var options = {
+  uri: 'https://api.cloudconvert.org/process',
+  json: {
+    "apikey": "<api key>",
+    "inputformat": "website",
+    "outputformat": "pdf"
+  }
+}
+
+request.post(options, function (error, response, body) {
+  if (error || response.statusCode !== 200) {
+    console.log('error', error, response.statusCode, body)
+  }
+  r = request.post('https:'+body.url, function(error, response, body) {
+    if (error) {// || response.statusCode !== 200) {
+      console.log('error', error, response, body)
+    }
+    console.log('response', body)
+    var file = fs.createWriteStream("simple.pdf");
+    http.get('http:' + JSON.parse(body).output.url, function(response) {
+      response.pipe(file);
+    });
+  })
+
+  var form = r.form()
+  form.append('input', 'upload')
+  form.append('outputformat', 'pdf')
+  form.append('file', fs.createReadStream(path.join(__dirname, 'simple.html')))
+
+
+})
